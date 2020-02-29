e44c2082b63437e1d64292c536569dabd6571df4
Wojtek Zalewski <wbzwojciechzalewski@gmail.com>
2020-02-29 08:54:03 +0100

    utworzenie modułu shared, dodanie SharedModule to tablicy imports w SpaceModule

--- src/app/space/space.module.ts
+++ src/app/space/space.module.ts
@@ -7,12 +7,14 @@ import { SpaceShipComponent } from './space-ship/space-ship.component';
 import { PilotComponent } from './pilot/pilot.component';
 import { PilotRoomComponent } from './pilot-room/pilot-room.component';
 import { EngineersRoomComponent } from './engineers-room/engineers-room.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     SpaceRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SharedModule
   ],
   declarations: [HangarComponent, SpaceShipComponent, PilotComponent, PilotRoomComponent, EngineersRoomComponent],
   exports: [
