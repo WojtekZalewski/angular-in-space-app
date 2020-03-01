0702db4eae11fe02153feaff4613fd147fb59b44
Wojtek Zalewski <wbzwojciechzalewski@gmail.com>
2020-03-01 18:31:46 +0100

    ustawienie focus testów, utworzenie klasy HttpClientMock, utworzenie instancji HttpClientMock oraz PilotService; przetestowanie metody getPilot, zamockowanie requestu HTTP, sprawdzenie czy request wysyłany jest pod prawidłowo, sprawdzenie czy metoda getPilot zwraca Observable z instancją pilota; dodanie grupy testów savePilot, sprawdzenie przypadku zapisu instniejącego pilota, przypadek zapisu nowego pilota

--- src/app/space/pilot.service.spec.ts
+++ src/app/space/pilot.service.spec.ts
@@ -1,12 +1,62 @@
-import { TestBed } from '@angular/core/testing';
-
+import { environment } from '../../environments/environment';
 import { PilotService } from './pilot.service';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
+import { Pilot, PilotAttrs } from './pilot';
+import { HttpClientMock } from 'src/testing/http-client-mock';
 
 describe('PilotService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let pilotService: PilotService;
+  let http: HttpClientMock;
 
-  it('should be created', () => {
-    const service: PilotService = TestBed.get(PilotService);
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    http = new HttpClientMock();
+    pilotService = new PilotService(http as HttpClient);
+  });
+
+  describe('getPilot', () => {
+    beforeEach(() => {
+      const pilotAttrs = {id: 1, firstName: 'Mike', lastName: 'Tomsky', imageUrl: ''};
+      spyOn(http, 'get').and.returnValue(of(pilotAttrs));
+    });
+
+    it('should make a request for pilot', () => {
+      pilotService.getPilot(1);
+      expect(http.get).toHaveBeenCalledWith(`${environment.apiUrl}/pilots/1`);
+    });
+
+    it('should return pilot object', () => {
+      pilotService.getPilot(1).subscribe((pilot) => {
+        expect(pilot instanceof  Pilot).toBeTruthy();
+      });
+    });
+  });
+
+  describe('savePilot', () => {
+    let pilotAttrs: PilotAttrs;
+
+    describe('when pilot has id', () => {
+      beforeEach(() => {
+        pilotAttrs = {id: 1, firstName: 'Mike', lastName: 'Tomsky', imageUrl: ''};
+        spyOn(http, 'put').and.returnValue(of(pilotAttrs));
+      });
+
+      it('should make post request', () => {
+        pilotService.savePilot(pilotAttrs);
+        expect(http.put).toHaveBeenCalledWith(`${environment.apiUrl}/pilots/1`, pilotAttrs);
+      });
+    });
+
+    describe('when pilot has not id', () => {
+      beforeEach(() => {
+        pilotAttrs = {id: null, firstName: 'Mike', lastName: 'Tomsky', imageUrl: ''};
+        spyOn(http, 'post').and.returnValue(of({...pilotAttrs, id: 1}));
+      });
+
+      it('should make put request', () => {
+        pilotService.savePilot(pilotAttrs);
+        expect(http.post).toHaveBeenCalledWith(`${environment.apiUrl}/pilots`, pilotAttrs);
+      });
+    });
   });
 });
