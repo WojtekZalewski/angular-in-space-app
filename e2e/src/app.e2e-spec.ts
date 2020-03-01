77dda0566529bcf7a71802549e3f1f611a48ed05
Wojtek Zalewski <wbzwojciechzalewski@gmail.com>
2020-03-01 20:49:49 +0100

    zmiana portu dla polecenia ng e2e; utworzenie pliku ship-production.e2e.-spec.ts i ship-production.po.ts, dodanie helperów, dodanie implementacji testów, otwarcie widoku hangaru w przeglądarce, określenie liczby produkowanych statków, określenie typu produkowanych statków, dodanie przycisku produkcji, sprawdzenie czy statek został utworzony

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to angular-in-space-app!');
+    expect(page.getParagraphText()).toEqual('Hangar');
   });
 });
